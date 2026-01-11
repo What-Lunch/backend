c128648398b06d461bdbc70de20f53127dd80a98
ramong26 <rlatndus21049@gmail.com>
2026-01-11 11:55:57 +0900

    fix: 빌드 에러 "build": "npx nest build",로 변경 - Render 서버에는 Nest CLI가 없음 - npx로 로컬에서 찾도록

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 void bootstrap();
