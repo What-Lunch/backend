7207f4f666719d286c027eebeb01d2676f21e090
ramong26 <rlatndus21049@gmail.com>
2026-01-13 21:42:55 +0900

    fix: env

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 }
 void bootstrap();
