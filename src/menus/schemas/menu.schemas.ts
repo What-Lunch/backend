f71af12cfa772f3cacf4d0fdc4fba1d0612315e7
Parkchanyoung0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2026-01-14 23:25:47 +0900

    fix: any 타입 제거

--- src/menus/schemas/menu.schemas.ts
+++ src/menus/schemas/menu.schemas.ts
@@ -3,6 +3,7 @@ import { Document } from 'mongoose';
 
 import { MenuCategory } from '../enum/menu-category.enum';
 import { MenuContext } from '../enum/menu-context.enum';
+import { Types } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class Menu extends Document {
@@ -34,9 +35,12 @@ export const MenuSchema = SchemaFactory.createForClass(Menu);
 
 MenuSchema.set('toJSON', {
   versionKey: false,
-  transform: (_doc, ret: any) => {
-    ret.id = ret._id;
-    delete ret._id;
-    return ret;
+  transform: (_doc, ret: Menu & { _id: Types.ObjectId }) => {
+    const { _id, ...rest } = ret;
+
+    return {
+      id: _id.toString(),
+      ...rest,
+    };
   },
 });
