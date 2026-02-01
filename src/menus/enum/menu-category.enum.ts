fc118a8d5ce9b29663ff460f97049a3bddc4d892
Parkchanyoung0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2026-02-01 21:42:01 +0900

    feat: 음식 메뉴 추가

--- src/menus/enum/menu-category.enum.ts
+++ src/menus/enum/menu-category.enum.ts
@@ -1,10 +1,10 @@
 // 메뉴 카테고리
 export enum MenuCategory {
-  ALL = 'all',
-  BEST = 'best',
-  KOREAN = 'korean',
-  CHINESE = 'chinese',
-  JAPANESE = 'japanese',
-  WESTERN = 'western',
-  SNACK = 'snack',
+  ALL = '전체',
+  BEST = '베스트',
+  KOREAN = '한식',
+  CHINESE = '중식',
+  JAPANESE = '일식',
+  WESTERN = '양식',
+  SNACK = '분식',
 }
